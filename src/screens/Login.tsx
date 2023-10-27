ea33f1087313b252fb52ce65329b5da8105be6a4
Abinash Karmakar <codeAbinash@gmail.com>
2023-10-27 16:37:15 +0530

    Added accent color

---
+++ src/screens/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="bg-accent">
+      <h1>This is Login Page </h1>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#F9A826",
+      },
+    },
   },
   plugins: [],
 };
