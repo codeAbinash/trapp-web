8609b37f6dfb3bed95fa6f249e4b17bba0921850
Abinash Karmakar <codeAbinash@gmail.com>
2023-11-07 18:37:09 +0530

    Add build and esbuild options to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+    pure: ['console.log'],
+  },
 })
