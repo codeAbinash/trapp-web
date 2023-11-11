f4e803ec42ff67132e9d48fbedc6dd2bb5e36662
codeAntu <codeantu@gmail.com>
2023-11-12 00:23:22 +0530

    Invert loading icon color in Loading and LoadingButton components

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 export function Loading() {
   return (
     <div className='screen flex items-center justify-center'>
-      <img src='/icons/other/loading.svg' className='w-10' />
+      <img src='/icons/other/loading.svg' className='w-10 invert' />
     </div>
   )
 }
@@ -9,7 +9,7 @@ export function Loading() {
 export function LoadingButton() {
   return (
     <div className='screen flex items-center justify-center py-4'>
-      <img src='/icons/other/loading.svg' className='w-6' />
+      <img src='/icons/other/loading.svg' className='w-6 invert' />
     </div>
   )
 }
