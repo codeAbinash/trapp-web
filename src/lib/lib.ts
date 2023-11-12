ed09b8cc1f8b6f35ded15296f2ed9ef377995b54
codeAntu <codeantu@gmail.com>
2023-11-12 12:40:56 +0530

    Add headers to fetch request in countryCode function

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -5,7 +5,12 @@ type CountryCode = keyof typeof countryCodes
 export async function countryCode() {
   try {
     const apiUrl = 'https://ip-api.com/json'
-    const res = await fetch(apiUrl)
+    const res = await fetch(apiUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
     if (res.status !== 200) return ''
     const data = await res.json()
     const code: CountryCode = data.countryCode
