d41813b505a409fea720f682051d0dc2fb00bad5
codeAntu <codeantu@gmail.com>
2023-11-12 12:30:26 +0530

    Update Content-Security-Policy and add Accept header to fetch request

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -5,7 +5,11 @@ type CountryCode = keyof typeof countryCodes
 export async function countryCode() {
   try {
     const apiUrl = 'http://ip-api.com/json'
-    const res = await fetch(apiUrl)
+    const res = await fetch(apiUrl, {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
     const data = await res.json()
     const code: CountryCode = data.countryCode
     return countryCodes[code]
