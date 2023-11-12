a49ebe76d87ed4667be1ec58fe1726d0f967b643
codeAntu <codeantu@gmail.com>
2023-11-12 21:52:05 +0530

    Fix login with component styling

--- src/screens/Auth/components/LoginWith.tsx
+++ src/screens/Auth/components/LoginWith.tsx
@@ -2,9 +2,9 @@ function LoginWith() {
   return (
     <div className='login-with flex w-full flex-col items-center justify-center gap-3'>
       <div className='flex w-full flex-row items-center justify-center'>
-        <div className='h-0.5 w-full bg-white/20'></div>
+        <div className='h-[1px] w-full bg-white/20'></div>
         <span className='w-full text-center text-xs'>Continue with</span>
-        <div className='h-0.5 w-full bg-white/20'></div>
+        <div className='h-[1px] w-full bg-white/20'></div>
       </div>
 
       <div className='flex gap-5'>
