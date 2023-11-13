abcb19da8b37068e1480fc77ebed3160e8864df0
codeAntu <codeantu@gmail.com>
2023-11-13 15:29:23 +0530

    Add service worker registration to static.js

--- public/static.js
+++ public/static.js
@@ -1,3 +1,16 @@
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      const registration = navigator.serviceWorker.register('sw.js')
+      console.log('Service Worker Registered')
+    } catch (error) {
+      console.warn('Error Registering Service Worker')
+      console.log(error)
+    }
+  } else console.log('Service worker is not available for this device')
+}
+registerSW()
+
 function absorbEvent_(event) {
   var e = event || window.event
   e.preventDefault && e.preventDefault()
---
+++ public/sw.js
@@ -0,0 +1,141 @@
+console.log('Service Worker Loaded...')
+
+const cacheData = {
+  showCacheThenFetch: {
+    name: 'trapp-show-cache-then-fetch-v1',
+    valid: 'trapp-show-cache-then-fetch',
+  },
+  emojiCache: {
+    name: 'emoji-cache-v1',
+    valid: 'emoji-cache',
+  },
+  fontCache: {
+    name: 'trapp-font-cache-v1',
+    valid: 'trapp-font-cache',
+  },
+  static: {
+    name: 'trapp-static-files-v1',
+    valid: 'trapp-static-files',
+    urls: [
+      // While loading don't load the spinner 😆🤣
+      // 'https://codeabinash.github.io/trapp/assets/spinner.svg',
+    ],
+  },
+}
+
+function existCacheName(cacheName) {
+  for (let key in cacheData)
+    if (cacheName.startsWith(cacheData[key].valid) && cacheName !== cacheData[key].name) return true
+  return false
+}
+
+self.addEventListener('install', (event) => {
+  console.log('Caching Shell Assets')
+  event.waitUntil(
+    caches.open(cacheData.static.name).then((cache) => {
+      console.log('Caching Shell Assets Done')
+      cache.addAll(cacheData.static.urls)
+    }),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker Activated')
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (!existCacheName(key)) return
+          console.log('Deleting old cache', key)
+          return caches.delete(key)
+        }),
+      ),
+    ),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  const url = event.request.url
+  // If the request includes the start link then clear the cache
+  console.log(url)
+  if (url.startsWith('https://dataabinash.github.io/emoji')) {
+    // Never load emojis again
+    event.respondWith(
+      caches.match(event.request).then((cacheResponse) => {
+        if (cacheResponse) return cacheResponse
+        const fetchUrl = fetch(event.request).then((fetchResponse) => {
+          return caches.open(cacheData.emojiCache.name).then((cache) => {
+            cache.put(event.request, fetchResponse.clone())
+            return fetchResponse
+          })
+        })
+        return cacheResponse || fetchUrl
+      }),
+    )
+  } else if (url.endsWith('.ttf') || url.endsWith('.woff2') || url.endsWith('.woff')) {
+    // Never load fonts again
+    event.respondWith(
+      caches.match(event.request).then((cacheResponse) => {
+        if (cacheResponse) return cacheResponse
+        const fetchUrl = fetch(event.request).then((fetchResponse) => {
+          return caches.open(cacheData.fontCache.name).then((cache) => {
+            cache.put(event.request, fetchResponse.clone())
+            return fetchResponse
+          })
+        })
+        return cacheResponse || fetchUrl
+      }),
+    )
+  }
+  //   else if (url.includes('https://trapp.vercel.app/')) {
+  // if (url.includes('trapp/icons/reset.svg')) {
+  //   console.log('Deleting all cache');
+  //   caches.keys().then((cacheNames) => {
+  //     console.log(cacheNames);
+  //     return Promise.all(
+  //       cacheNames.map((cacheName) => {
+  //         return caches.delete(cacheName);
+  //       }),
+  //     );
+  //   });
+  // } else
+  //  event.respondWith(
+  //    caches.match(event.request).then((cacheResponse) => {
+  //      const fetchUrl = fetch(event.request).then((fetchResponse) => {
+  //        return caches.open(cacheData.showCacheThenFetch.name).then((cache) => {
+  //          cache.put(event.request, fetchResponse.clone())
+  //          return fetchResponse
+  //        })
+  //      })
+  //      return cacheResponse || fetchUrl
+  //    }),
+  //  )
+  //   }
+  else if (
+    (url.includes('https://trapp-sigma.vercel.app') || url.includes('http://localhost')) &&
+    (url.endsWith('.png') ||
+      url.endsWith('.jpg') ||
+      url.endsWith('.jpeg') ||
+      url.endsWith('.svg') ||
+      url.endsWith('.gif') ||
+      url.endsWith('.ico') ||
+      url.endsWith('.webp'))
+  ) {
+    console.log('Image')
+
+    event.respondWith(
+      caches.match(event.request).then((cacheResponse) => {
+        const fetchUrl = fetch(event.request).then((fetchResponse) => {
+          return caches.open(cacheData.showCacheThenFetch.name).then((cache) => {
+            cache.put(event.request, fetchResponse.clone())
+            return fetchResponse
+          })
+        })
+        return cacheResponse || fetchUrl
+      }),
+    )
+  } else {
+    // Always load from internet
+    event.respondWith(fetch(event.request))
+  }
+})
