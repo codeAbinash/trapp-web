a29312847eb906047b41d4d7cf15cf648ebc9df4
Abinash Karmakar <codeAbinash@gmail.com>
2024-07-27 21:11:09 +0530

    If website is open with the browser, redirect to play store

--- public/static.js
+++ public/static.js
@@ -1,24 +1,35 @@
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      const registration = navigator.serviceWorker.register('sw.js')
-      console.log('Service Worker Registered')
-    } catch (error) {
-      console.warn('Error Registering Service Worker')
-      console.log(error)
-    }
-  } else console.log('Service worker is not available for this device')
-}
-// registerSW()
+// async function registerSW() {
+//   if ('serviceWorker' in navigator) {
+//     try {
+//       const registration = navigator.serviceWorker.register('sw.js')
+//       console.log('Service Worker Registered')
+//     } catch (error) {
+//       console.warn('Error Registering Service Worker')
+//       console.log(error)
+//     }
+//   } else console.log('Service worker is not available for this device')
+// }
+// // registerSW()
 
-function absorbEvent_(event) {
-  var e = event || window.event
-  e.preventDefault && e.preventDefault()
-  e.stopPropagation && e.stopPropagation()
-  e.cancelBubble = true
-  e.returnValue = false
-  return false
-}
+// function absorbEvent_(event) {
+//   var e = event || window.event
+//   e.preventDefault && e.preventDefault()
+//   e.stopPropagation && e.stopPropagation()
+//   e.cancelBubble = true
+//   e.returnValue = false
+//   return false
+// }
+
+
+window.isApp = true
+setTimeout(() => {
+  if (window.isApp) {
+    console.log('App Detected')
+  } else {
+    console.log('Not App')
+    window.location.href = 'https://play.google.com/store/apps/details?id=com.trapp.app'
+  }
+}, 100)
 
 // Disable Context Menu
 // window.addEventListener('load', () => {
